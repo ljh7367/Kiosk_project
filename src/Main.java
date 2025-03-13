9d55af22b2fc084c9e18e51631220927675f545e
Jong Hyeon Lee <ljh0990@naver.com>
2025-03-13 21:04:24 +0900

    lv2 과제 완료

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        List<MenuItem> menuItems = new ArrayList<>(); //리스트
+        menuItems.add(new MenuItem("ShakeBuger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-
-
-            List<MenuItem> menuItems = new ArrayList<>();
-            menuItems.add(new MenuItem("ShakeBuger"  , 6.9 , "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("SmokeShack"  , 8.9 , "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("Cheeseburger" , 6.9 , "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("Hamburger" , 5.4 , "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("[SHAKESHACK MENU]");
-        for(MenuItem menu : menuItems) {
+        for (MenuItem menu : menuItems) {
             System.out.println(menu);
         }
 
 
-            System.out.println("0. 종료      | 종료");
-            System.out.print("번호를 선택해주세요: ");
+        System.out.println("0. 종료      | 종료");
+        System.out.print("번호를 선택해주세요: ");
 
         int choice = scanner.nextInt();
 
-        switch(choice) { // 조건문을 넣어 참과 거짓을 통하여 다음 선택지로 이동할수 있게 만들었다.
+
+        switch (choice) { // 조건문을 넣어 참과 거짓을 통하여 다음 선택지로 이동할수 있게 만들었다.
             case 1:
+                System.out.println(menuItems.get(0));
+                System.out.print("수량: ");
+                break;
             case 2:
+                System.out.println(menuItems.get(1));
+                System.out.print("수량: ");
+                break;
             case 3:
+                System.out.println(menuItems.get(0));
+                System.out.print("수량: ");
+                break;
             case 4:
-            System.out.print("주문 수량: ");
-
+                System.out.println(menuItems.get(0));
+                System.out.print("수량: ");
+                break;
             case 0:
-            System.out.println("프로그램을 종료합니다."); // 0번을 눌렀을 경우에 키오스크가 종료되도록 한다.
-
+                System.out.println("프로그램을 종료합니다."); // 0번을 눌렀을 경우에 키오스크가 종료되도록 한다.
+                break;
             default:
-            System.out.println("잘못선택하셨습니다. 다시 선택해주세요."); // 마지막에 else문을 넣어 거짓일 경우 SHAKESHACK MENU로 다시 이동하게 break문은 뺀다.
+                System.out.println("잘못선택하셨습니다. 다시 선택해주세요."); // 마지막에 else문을 넣어 거짓일 경우 SHAKESHACK MENU로 다시 이동하게 break문은 뺀다.
+
         }
 
 
-        }
     }
+}
 
--- src/MenuItem.java
+++ src/MenuItem.java
@@ -10,20 +10,30 @@ public class MenuItem {
         this.explanation = explanation;
     }
 
-        public String getName() {
-            return this.name;
-        }
-        public double getPrice() {
-            return this.price;
-    }
-        public String getExplanation(){
-            return this.explanation;
-
+    public String getName() {
+        return this.name;
     }
 
-
-
+    public double getPrice() {
+        return this.price;
     }
+
+    public String getExplanation() {
+        return this.explanation;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | W %.1f | %s", this.name, this.price, this.explanation);
+    }     // 문자열을 추가하기 위해서는 %s , doubled을 사용하려했지만 오류가남. 1f는 소숫점 1자리까지 표기
+
+}
+
+
+
+
+
+
 
 
 
